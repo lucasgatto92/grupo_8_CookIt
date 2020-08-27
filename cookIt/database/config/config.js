2b07c40c5936b8656cd69cd6798a70178224bdf7
Eric <menaericdaniel@gmail.com>
2020-08-27 09:29:10 -0300

    base de datos actualizada

--- cookIt/database/config/config.js
+++ cookIt/database/config/config.js
@@ -1,10 +1,11 @@
 module.exports = {
     "development": {
         "username": "root",
-        "password": "",
+        "password": "root",
         "database": "cookit",
         "host": "127.0.0.1",
         "dialect": "mysql",
+        "port": "8889"
     },
     "test": {
         "username": "root",
