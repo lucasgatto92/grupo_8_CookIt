06524cef165ceb4ad0160cc64ff94486e63ec8b2
LupitaFernandez <fernandez.guada@hotmail.com>
2020-09-17 08:28:05 -0300

    Ruta

--- cookIt/routes/admin.js
+++ cookIt/routes/admin.js
@@ -13,7 +13,7 @@ const productosController = require('../controllers/productosController');
 router.get('/products', adminCheck, adminController.list);
 router.get('/products/add', adminCheck, adminController.add);
 router.post('/products/save', adminCheck, productMulter.any(), adminController.save);
-router.get('/products/:id/edit', adminCheck, adminController.edit);
+router.get('/productos/:id/edit', adminCheck, adminController.edit);
 router.put('/products/:id', adminCheck, productMulter.any(), adminController.update);
 router.delete('/products/:id', adminController.delete);
 
